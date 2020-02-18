3bc421709c2bc155edd478af61203c233ff2aeb0
avyjyo11 <avy.jyo11@gmail.com>
2020-02-18 21:13:41 +0545

    Updates

---
+++ precache-manifest.fc4a961dc336287910a9e1ba8136304f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682e39219972846eb92b63126b8a0f6a",
+    "url": "/picpost-frontend-react/index.html"
+  },
+  {
+    "revision": "3c59767ade7b2e43a31e",
+    "url": "/picpost-frontend-react/static/css/main.8d4d1460.chunk.css"
+  },
+  {
+    "revision": "c4067eceab00e4346869",
+    "url": "/picpost-frontend-react/static/js/2.e01accbe.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/picpost-frontend-react/static/js/2.e01accbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c59767ade7b2e43a31e",
+    "url": "/picpost-frontend-react/static/js/main.a42b74da.chunk.js"
+  },
+  {
+    "revision": "f3940b2ee8d3396398f2",
+    "url": "/picpost-frontend-react/static/js/runtime-main.b2d11886.js"
+  }
+]);
